d81e3ac8597ffb0e5a260e2327d8af1b28500fbe
Lasse Zimmermann <fuersstudiumiid@gmail.com>
2024-11-29 03:11:17 +0100

    Added Quit function

--- src/graphicComponents/TestController.java
+++ src/graphicComponents/TestController.java
@@ -9,6 +9,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -19,29 +21,44 @@ public class TestController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	private TextField changeUsername;
+	
+	@FXML
+	private AnchorPane scenePane;
 
 	public void switchToCredits(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("Credits.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
-        String css = this.getClass().getResource("Style.css").toExternalForm();
-        scene.getStylesheets().add(css);
+		String css = this.getClass().getResource("Style.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		stage.show();
 	}
 
 	public void switchToMainmenu(ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("testoffx.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
-        String css = this.getClass().getResource("Style.css").toExternalForm();
-        scene.getStylesheets().add(css);
+		String css = this.getClass().getResource("Style.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		stage.show();
 	}
 
+	public void quitApplication(ActionEvent e) {
+		stage = (Stage) scenePane.getScene().getWindow();
+		stage.close();
+	}
+	
+	//TODO Get this working
+	public void getUsername(ActionEvent event) {
+		String username = changeUsername.getText();
+		System.out.println(username);
+	}
+
 	public void exampleMethod(ActionEvent e) {
-		
+
 	}
 
 }
--- src/graphicComponents/TestJavaFx.java
+++ src/graphicComponents/TestJavaFx.java
@@ -4,24 +4,41 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class TestJavaFx extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("testoffx.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(scene);
-        String css = this.getClass().getResource("Style.css").toExternalForm();
-        scene.getStylesheets().add(css);
-        //scene2.getStylesheets().add(css);
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Hello World");
+		primaryStage.setScene(scene);
+		String css = this.getClass().getResource("Style.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		// scene2.getStylesheets().add(css);
+		primaryStage.setOnCloseRequest(event -> {
+			event.consume();
+			quitApplication(primaryStage);
+		});
+		primaryStage.show();
+	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	// Close application on red cross
+	public void quitApplication(Stage stage) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Quit Program");
+		alert.setHeaderText("Quit Application?");
+
+		if (alert.showAndWait().get() == ButtonType.OK) {
+			stage.close();
+		}
+	}
 }
